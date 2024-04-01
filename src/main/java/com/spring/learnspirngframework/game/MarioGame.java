7029c430b8efa0cf6ca9292850a4ee97ed434080
변민우 <yja05092@gmail.com>
2024-04-01 17:32:01 +0900

    컴포넌트에 대한 Primary & Qualifier 어노테이션

--- src/main/java/com/spring/learnspirngframework/game/MarioGame.java
+++ src/main/java/com/spring/learnspirngframework/game/MarioGame.java
@@ -1,5 +1,10 @@
 package com.spring.learnspirngframework.game;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary // 여러 게임 중 마리오 게임을 기본으로 설정
 public class MarioGame implements GamingConsole {
     public void up() {
         System.out.println("Jump!");
--- src/main/java/com/spring/learnspirngframework/game/SuperContraGame.java
+++ src/main/java/com/spring/learnspirngframework/game/SuperContraGame.java
@@ -1,5 +1,10 @@
 package com.spring.learnspirngframework.game;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier("SuperContraGameQualifier") // Qualifier 데코레이션을 이용하면 특정 컴포넌트를 지정해서 사용할 수 있다.
 public class SuperContraGame implements GamingConsole {
     public void up() {
         System.out.println("Jump!");
