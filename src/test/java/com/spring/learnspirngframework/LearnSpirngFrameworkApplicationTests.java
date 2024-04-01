3e6a630c38156fc13f9b1cdd19b6ba0d9cfc2389
변민우 <yja05092@gmail.com>
2024-04-01 11:48:55 +0900

    Initial commit

---
+++ src/test/java/com/spring/learnspirngframework/LearnSpirngFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.learnspirngframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpirngFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
