4de014e3f9274690cf27a058feda39a08506aa25
변민우 <yja05092@gmail.com>
2024-04-01 14:13:48 +0900

    Spring Java 설정 파일에서 더 않은 Java Spring Bean 만들기

--- src/main/java/com/spring/learnspirngframework/App02HelloWorldSpring.java
+++ src/main/java/com/spring/learnspirngframework/App02HelloWorldSpring.java
@@ -7,5 +7,8 @@ public class App02HelloWorldSpring {
         var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
 //        getBean("name") 이런 식으로 메소드 이름을 지정해서 가져올 수 있다.
         System.out.println(context.getBean("name"));
+        System.out.println(context.getBean("age"));
+        System.out.println(context.getBean("person"));
+        System.out.println(context.getBean("address"));
     }
 }
--- src/main/java/com/spring/learnspirngframework/HelloWorldConfiguration.java
+++ src/main/java/com/spring/learnspirngframework/HelloWorldConfiguration.java
@@ -3,10 +3,33 @@ package com.spring.learnspirngframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
+record Person(String name, int age) {
+}
+
+record Address(String firstLine, String city) {
+}
+
+
 @Configuration
 public class HelloWorldConfiguration {
     @Bean
     public String name() {
         return "Min";
+    }
+
+    @Bean
+    public int age() {
+        return 15;
+    }
+
+    @Bean
+    public Person person() {
+        return new Person("byun", 20);
+    }
+
+    @Bean
+    public Address address() {
+        return new Address("가산동", "서울특별시");
     }
 }
